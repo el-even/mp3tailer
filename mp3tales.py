87c707a050bb2aa1cec10c0526fbec346418e20c
aberezovsky <aberezovskiy@aberezovskiy-pc.thumbtack.lo>
2015-09-30 16:48:58 +0600

    Cleared unused code

--- mp3tales.py
+++ mp3tales.py
@@ -10,15 +10,12 @@ last = 4
 fetched_path = "../fetched/"
 downloads_path = "../files/"
 
-# def formattedpath(divider):
-#     return size + divider + background + divider + textcolor
 
 if not os.path.exists(fetched_path):
     os.makedirs(fetched_path)
 
 if not os.path.exists(downloads_path):
     os.makedirs(downloads_path)
-
 
 
 def encoder(cp1251_file, utf8_file):
@@ -54,9 +51,6 @@ def body():
         fetcher(url, filename)
         encoder(filename, out_file)
 
-        # output=open(filename, 'rb')
-        # print output.read()
-
 
 def getName(remote_file):
     try:
@@ -66,52 +60,38 @@ def getName(remote_file):
     return result
     
 
-
 def download(url):
     remote_file = urllib2.urlopen(url)
-    # print remote_file.info()
-    # print remote_file.info()
     if getName(remote_file) != "":
         file_name = getName(remote_file)
     else:
         file_name = url.split('/')[-1]
     print file_name
-    # local_file = open(file_name, 'wb')
-    # local_file.write(remote_file.read())
-    # local_file.close()
 
-    
-    
-    # remote_file = urllib2.urlopen(url)
-    local_file = open(file_name, 'wb')
+    local_file = open(downloads_path+file_name, 'wb')
     meta = remote_file.info()
 
     file_size = int(meta.getheaders("Content-Length")[0])
-    #print "Downloading %s" % (file_name.split('/')[-1])
+    print "Downloading %s" % (file_name.split('/')[-1])
 
-    # print meta.getheaders("Content-Disposition")[0]
+    print meta.getheaders("Content-Disposition")[0]
 
-    # file_size_dl = 0
-    # block_sz = 8*1024
-    # while True:
-    #     buffer = remote_file.read(block_sz)
-    #     if not buffer:
-    #         break
+    file_size_dl = 0
+    block_sz = 8*1024
+    while True:
+        buffer = remote_file.read(block_sz)
+        if not buffer:
+            break
 
-    #     file_size_dl += len(buffer)
-    #     local_file.write(buffer)
-    #     status = r"%d of %d KB  [%3.2f%%]" % (file_size_dl/1024, file_size/1024, file_size_dl * 100. / file_size)
-    #     status = status + chr(8)*(len(status)+1)
-    #     print status,
-
-    # s = 'attachment; filename="tone24bit.m4a"'
-    # result = re.search('filename="(.*)"', meta.getheaders("Content-Disposition")[0])
-    # print result.group(1)
+        file_size_dl += len(buffer)
+        local_file.write(buffer)
+        status = r"%d of %d KB  [%3.2f%%]" % (file_size_dl/1024, file_size/1024, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
 
     local_file.close()
 
 
-
 download("http://download.linnrecords.com/test/m4a/tone24bit.aspx")
-download("http://mp3tales.info/audio/prikljuchenija_buratino.mp3")
+# download("http://mp3tales.info/audio/prikljuchenija_buratino.mp3")
 
