9c681eb62f26e6a9cef6b3b19f4ee97899cf3ed6
el_even <kpl@list.ru>
2015-10-03 22:14:26 +0600

    Refactoring: moved functions and settings outside

--- mp3tales.py
+++ mp3tales.py
@@ -1,14 +1,10 @@
 import urllib
 import urllib2
 import os
-import re
+# import re
 
-
-rooturl = "http://mp3tales.info/tales/"
-first = 3
-last = 4
-fetched_path = "../fetched/"
-downloads_path = "../files/"
+from download import *
+from config import *
 
 
 if not os.path.exists(fetched_path):
@@ -52,36 +48,7 @@ def body():
         encoder(filename, out_file)
 
 
-def download(url):
-    remote_file = urllib2.urlopen(url)
-    meta = remote_file.info()
-
-    try:
-        file_name = re.search('filename="(.*)"', meta.getheaders("Content-Disposition")[0]).group(1)
-    except:
-        file_name = url.split('/')[-1]
-
-    local_file = open(downloads_path+file_name, 'wb')
-
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading %s" % (file_name.split('/')[-1])
-
-    file_size_dl = 0
-    block_sz = 8*1024
-    while True:
-        buffer = remote_file.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        local_file.write(buffer)
-        status = r"%d of %d KB  [%3.2f%%]" % (file_size_dl/1024, file_size/1024, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print status,
-
-    local_file.close()
-
-
 download("http://download.linnrecords.com/test/m4a/tone24bit.aspx")
 # download("http://mp3tales.info/audio/prikljuchenija_buratino.mp3")
+
 
