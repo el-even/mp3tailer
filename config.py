da2b7f204953faa70d200f4505ea0549c2958b9b
aberezovsky <aberezovskiy@aberezovskiy-pc.thumbtack.lo>
2015-10-05 20:09:59 +0600

    Moved parser settings to config

--- config.py
+++ config.py
@@ -3,5 +3,12 @@ talesurl = rooturl + "tales/"
 fetched_path = "../fetched/"
 downloads_path = "../files/"
 database = "../tales.db"
-first = 49
+first = 48
 last = 49
+
+re_img_url = 'img class="saturate" src="/(.*?)" alt'
+re_mp3_url = '<source src="/(.*)" type="audio/mpeg">'
+re_title = '<h1 itemprop="name">(.*?)</h1>'
+re_year = "<meta itemprop='dateCreated' content='(.*?)'>"
+re_annotation = "(?s)<p>.*?<small>(.*?)</small>"
+re_description = '(?s)<p id="description".*?>(.*?)</p>'
--- parser.py
+++ parser.py
@@ -3,20 +3,23 @@ import re
 from config import *
 
 
-filename = fetched_path+"49_u.txt"
+filename = fetched_path+"48_u.txt"
 html = open(filename, 'r').read()
 
 def parser(html):
-    img_url = rooturl + re.search('img class="saturate" src="/(.*?)" alt', html).group(1)
-    mp3_url = rooturl + re.search('<source src="/(.*)" type="audio/mpeg">', html).group(1)
-    title = re.search('<h1 itemprop="name">(.*?)</h1>', html).group(1)
-    year = re.search("<meta itemprop='dateCreated' content='(.*?)'>", html).group(1)
-    description = re.search('<p id="description"(.|\n)*?>(.*)</p>', html).group(0)
+    img_url = rooturl + re.search(re_img_url, html).group(1)
+    mp3_url = rooturl + re.search(re_mp3_url, html).group(1)
+    title = re.search(re_title, html).group(1)
+    year = re.search(re_year, html).group(1)
+    annotation = re.search(re_annotation, html).group(1)
+    description = re.search(re_description, html).group(1)
+
 
     print img_url
     print mp3_url
     print title
     print year
-    print description
+    print annotation
+    # print description
 
 parser(html)
