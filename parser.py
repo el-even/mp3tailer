afbe034a8f39c80d39ab74ccf06dfb702375a2a2
el_even <kpl@list.ru>
2015-10-06 00:21:42 +0600

    Improved parser: strip spaces and tags

--- parser.py
+++ parser.py
@@ -6,13 +6,17 @@ from config import *
 filename = fetched_path+"48_u.txt"
 html = open(filename, 'r').read()
 
+
+def strip(text):
+    return re.sub("^\s*|\s*$|\s*?<br/>", "", text)
+
 def parser(html):
     img_url = rooturl + re.search(re_img_url, html).group(1)
     mp3_url = rooturl + re.search(re_mp3_url, html).group(1)
     title = re.search(re_title, html).group(1)
     year = re.search(re_year, html).group(1)
-    annotation = re.search(re_annotation, html).group(1)
-    description = re.search(re_description, html).group(1)
+    annotation = strip(re.search(re_annotation, html).group(1))
+    description = strip(re.search(re_description, html).group(1))
 
 
     print img_url
@@ -20,6 +24,6 @@ def parser(html):
     print title
     print year
     print annotation
-    # print description
+    print description
 
 parser(html)
