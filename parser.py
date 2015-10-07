ba16e23b74cd52726a3490dff769ae5580e9a328
aberezovsky <aberezovskiy@aberezovskiy-pc.thumbtack.lo>
2015-10-07 18:32:28 +0600

    Refined filename handling -- now without extention

--- parser.py
+++ parser.py
@@ -9,14 +9,15 @@ def strip(text):
 def parser(html):
     img_url = rooturl + re.search(re_img_url, html).group(1)
     mp3_url = rooturl + re.search(re_mp3_url, html).group(1)
+    filename = (mp3_url.split('/')[-1]).split('.')[0]
     title = re.search(re_title, html).group(1)
     year = re.search(re_year, html).group(1)
     annotation = strip(re.search(re_annotation, html).group(1))
     description = strip(re.search(re_description, html).group(1))
 
-
     print img_url
     print mp3_url
+    print filename
     print title
     print year
     print annotation
