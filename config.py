dc2fe7c633e47bf94e797eeddccddd9e592ebd88
el_even <kpl@list.ru>
2015-10-10 19:57:30 +0600

    Fixed img regexp -- now should work with tales without covers

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ talesurl = rooturl + "tales/"
 downloads_path = "../files/"
 database = "../tales.db"
 
-first_tale = 49
-last_tale = 52
+first_tale = 265
+last_tale = 267
 max_errors_allowed = 5
 
 ok_mark = "-- ok"
 
-re_img_url = 'img class="saturate" src="/(.*?)" alt'
+re_img_url = '(?s)<div class=\"row">.*?<img.*?src="/(.*?)"'
 re_mp3_url = '<source src="/(.*)" type="audio/mpeg">'
 re_title = '<h1 itemprop="name">(.*?)</h1>'
 re_year = "<meta itemprop='dateCreated' content='(.*?)'>"
